8617e550aaaef21b34d0641b921b30e1986156c1
Jirka Koutný <jirka@Jirka-iMac.local>
2016-11-18 13:35:30 +0100

    Init commmit

---
+++ azure_d2c_firebase_persistor.js
@@ -0,0 +1,51 @@
+'use strict';
+
+// Require
+var nconf = require('nconf');
+var iotHub = require('azure-event-hubs').Client;
+var firebase = require("firebase");
+
+// Configuration
+nconf.argv().env().file('./config.json');
+
+// Firebase init
+var firebaseConfig = {
+    apiKey: nconf.get('firebaseApiKey'),
+    databaseURL: nconf.get('firebaseDatabaseURL'),
+};
+firebase.initializeApp(firebaseConfig);
+
+// IoT Hub init
+var iotHubConnString = nconf.get('iotHubConnString');
+var iotHubClient = iotHub.fromConnectionString(iotHubConnString);
+
+// Azure D2C Firebase Persistor
+iotHubClient.open()
+    .then(iotHubClient.getPartitionIds.bind(iotHubClient))
+    .then(function (partitionIds) {
+        return partitionIds.map(function (partitionId) {
+            return iotHubClient.createReceiver('$Default', partitionId, { 'startAfterTime': Date.now() }).then(function (receiver) {
+                console.log('Created partition receiver: ' + partitionId)
+                receiver.on('errorReceived', printError);
+                receiver.on('message', printMessage);
+            });
+        });
+    })
+    .catch(printError);
+
+function persistMessage(message) {
+    var deviceMessagesNode = firebase.database().ref('messages');
+    deviceMessagesNode.push(message);
+};
+
+// Other methods
+var printError = function (err) {
+    console.log(err.message);
+};
+
+var printMessage = function (message) {
+    console.log('Message received: ');
+    console.log(JSON.stringify(message.body));
+    persistMessage(message.body)
+    console.log('');
+};
